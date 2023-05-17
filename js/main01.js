69e3821fde60594bea983d21289b4ad0833dc553
yunkyung <133613667+Yunkyungii@users.noreply.github.com>
2023-05-17 17:12:09 +0900

    html basic
    html basic

---
+++ js/main01.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.mainSlide').slick({
+        // arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+    });
+})
+// 옵션 단어는 slick 전용 단어임. 찾아보고 필요한 기능 사용할 것.
